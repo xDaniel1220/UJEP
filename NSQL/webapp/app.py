461027646b74c257e4787e8273ac8055b593b0db
Daniel Bláha <daniel.blaha@hotmail.com>
2023-12-13 22:06:47 +0100

    flask user authentication
    - created a user authentication system in app.py for new users to register and old users to login
    - database.db file created in the webapp directory
    - added new templates for all the existing and newly created routes
    - new route /dashboard
    - use of sqlite for the new authentication system
    - /redis route deleted for now
    - debug mode activated

--- NSQL/webapp/app.py
+++ NSQL/webapp/app.py
@@ -1,28 +1,70 @@
 from flask import Flask, render_template, url_for, request, redirect, session
 from redis import Redis
 from pymongo import MongoClient
+from flask_sqlalchemy import SQLAlchemy
+import bcrypt
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'secret_key'
+db = SQLAlchemy(app)
+
 redis = Redis(host='redis', port=6379)
 mongo = MongoClient("mongodb://mongodb:27017", connect=False)
 
-@app.route('/')
-def index():
-    if 'username' in session:
-        return 'Get out of here ' + session['username']
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.checkpw(password.encode('utf-8'), self.password.encode('utf-8'))
     
-    return render_template('index.html', slur = 'a charm')
+with app.app_context():
+    db.create_all()
 
-@app.route('/redis')
-def redis_route():
-    redis.incr('hits')
-    counter = str(redis.get('hits'), 'utf-8')
-    return "This web page has been viewed " + counter + " time(s)"
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template('index.html')
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.check_password(password):
+            session['username'] = user.username
+            session['password'] = user.password
+            return redirect('/dashboard')
+        else:
+            return render_template('login.html', error='Inavlid user')
+        
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET','POST'])
 def register():
-    if request.method == 'GET':
-        mongo.db.users.insert_one({"name":"Daniel Blaha"})
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect('/login')
+    return render_template('register.html')
+
+@app.route('/dashboard')
+def dashboard():
+    if session['username']:
+        user = User.query.filter_by(username=session['username']).first()
+        return render_template('dashboard.html', user=user)
+    return redirect('/login')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=5001)
