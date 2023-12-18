a0679390acc7aba993baf6d8f995be1c0c8ab246
Daniel Bláha <daniel.blaha@hotmail.com>
2023-12-18 11:36:02 +0100

    RDBS: SQLAlchemy integration
    - SQLAlchemy now successfully executes 'select' statement in postgresql running on localhost.

--- RDBS/database.py
+++ RDBS/database.py
@@ -1,17 +1,36 @@
-from sqlalchemy import create_engine
-# for postgreSQL database credentials can be written as 
+from sqlalchemy import create_engine, select, MetaData, Table, Column, Integer, String
+
 user = 'daniel.blaha'
 password = ''
 host = 'localhost'
 port = '5432'
 database = 'starfield'
-# for creating connection string
+
 connection_str = f'postgresql://{user}:{password}@{host}:{port}/{database}'
-# SQLAlchemy engine
 engine = create_engine(connection_str)
-# you can test if the connection is made or not
+metadata_obj = MetaData()
+
 try:
-    with engine.connect() as connection_str:
-        print('Successfully connected to the PostgreSQL database')
+    with engine.connect() as connection:
+        print('Successfully connected to the PostgreSQL database.')
 except Exception as ex:
     print(f'Sorry failed to connect: {ex}')
+
+table = Table(
+    "systemy",
+    metadata_obj,
+    Column("id_systemy", Integer, primary_key=True),
+    Column("nazev", String(50))
+)
+
+stmt = select(table).where(table.c.nazev == "Sol")
+print(stmt)
+
+with engine.connect() as conn:
+    for row in conn.execute(stmt):
+        print(row)
+
+
+
+
+
